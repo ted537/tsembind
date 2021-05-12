12aa81890605c8ff6d05f6b718f7a280a58587bc
Ted Brownlow <ted.brownlow@gmail.com>
2021-05-12 18:02:42 -0400

    fix automatic output filename generation

--- src/cmd.js
+++ src/cmd.js
@@ -9,14 +9,19 @@ program
 	.requiredOption('-i, --input <lib.js>', 'your input embind library')
 	.option('-o, --output <lib.d.ts>', '.d.ts output location')
 
+function changeExtension(filename, ext) {
+	const {dir,name} = path.parse(filename)
+	return path.format({dir,name,ext})
+}
+
 program.parse(process.argv);
 const {input,output} = program.opts();
 if (!fs.existsSync(input)) {
 	throw `Input file ${input} does not exist!!!`
 }
-const actualOutput = output ? output : path.basename(input,'.js') + '.d.ts'
+const actualOutput = output ? output : changeExtension(input,'.d.ts')
 console.log('writing output to '+actualOutput)
 
 generateTypescriptBindings(process.cwd()+'/'+input).then(
-	bindings => fs.writeFileSync(output, bindings)
+	bindings => fs.writeFileSync(actualOutput, bindings)
 );
