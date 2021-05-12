48d500dc8fc5524455de5dd47fe1c17c5139732d
Ted Brownlow <ted.brownlow@gmail.com>
2021-05-12 13:15:15 -0400

    get registry back from module

--- src/tsembind.js
+++ src/tsembind.js
@@ -1,22 +1,47 @@
-const wrapRegisterFunction = f => (...args) => {
+const registries = new Map();
+
+const wrapRegisterFunction = (registry,f) => function(...args) {
 	console.log("registering function")
+	registry.functions.push({
+		returnType:args[0],
+		parameterTypes: args.slice(1)
+	})
 	return f(...args)
 }
 
-const wrapRegisterClass = f => (...args) => {
+const wrapRegisterClass = (registry,f) => (...args) => {
 	console.log("registering class")
 	return f(...args)
 }
 
-const injectBindings = env =>
-	({...env,
+const injectBindings = info => {
+	const registry = {functions:[], classes:[]}
+	const {
+		_embind_register_function,
+		_embind_register_class
+	} = info.env;
+	const injectedEnv = {...info.env,
 		_embind_register_function: 
-			wrapRegisterFunction(env._embind_register_function),
+			wrapRegisterFunction(registry,_embind_register_function),
 		_embind_register_class:
-			wrapRegisterClass(env._embind_register_class)
-	})
+			wrapRegisterClass(registry,_embind_register_class)
+	}
+	const injectedInfo = {...info,env:injectedEnv}
+	return {registry, injectedInfo}
+}
 
-const wrapWebAssemblyInit = originalFunction => (binary,info) =>
-	originalFunction(binary,{...info,env:injectBindings(info.env)})
+const wrapWebAssemblyInit = originalFunction => async (binary,info) => {
+	const {registry,injectedInfo} = injectBindings(info)
+	const result = await originalFunction(binary,injectedInfo)
+	const {module,instance} = result;
+	registries.set(instance.exports,registry)
+	return result;
+}
 
-WebAssembly.instantiate = wrapWebAssemblyInit(WebAssembly.instantiate)
+const inject = () =>
+	WebAssembly.instantiate = wrapWebAssemblyInit(WebAssembly.instantiate)
+
+const getDeclarations = module =>
+	registries.get(module.asm)
+
+module.exports = {inject, getDeclarations}
--- test/runsimplefunc.js
+++ test/runsimplefunc.js
@@ -1,5 +1,7 @@
-require('../src/tsembind.js')
+const {inject, getDeclarations} = require('../src/tsembind.js')
+inject();
 async function main() {
 	const module = await require('../build/simplefunc.js')();
+	console.log(getDeclarations(module))
 }
 main()
