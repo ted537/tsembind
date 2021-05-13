c12dd1df77a17e9c128b609cff18037da1be46d6
Ted Brownlow <ted.brownlow@gmail.com>
2021-05-13 14:18:54 -0400

    add test for returning int constant

--- test/test.js
+++ test/test.js
@@ -5,10 +5,21 @@ const lastLine = text =>
 	text.slice(text.lastIndexOf('\n')+1)
 
 describe('getTypescriptBindings', ()=> {
-	it('should detect that a void function exists', async ()=>{
+	it('should generate a void function declaration', async ()=>{
 		assert.equal(
 			lastLine(await generateTypescriptBindings('lib/voidfunc.js')),
 			'declare function f(): void;'
 		)
 	} )
+})
+
+describe('getTypescriptBindings', ()=> {
+	it('should generate a declaration for a function returning a constant', 
+		async ()=>{
+			assert.equal(
+				lastLine(await generateTypescriptBindings('lib/return_int.js')),
+				'declare function f(): Int;'
+			)
+		}
+	)
 })
