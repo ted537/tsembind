eb900479782a720dd229749c82551fdbd4753ba9
Ted Brownlow <ted.brownlow@gmail.com>
2021-05-15 13:33:37 -0400

    fix function tests

--- test/functions.js
+++ test/functions.js
@@ -1,90 +1,89 @@
 const {generateTypescriptBindings} = require('../src/tsembind.js')
 const assert = require('assert')
 
-const lastLine = text =>
-	text.slice(text.lastIndexOf('\n')+1)
-
-const lastLines = n => text => {
-	const lines = text.split('\n');
-	return lines.slice(lines.length-n).join('\n')
+const findFunctions = text => {
+	const lines = text.split('\n')
+	return lines.filter(
+		line=>line.includes('(') && !line.includes('declare ')
+	).join('\n')
 }
 
 describe('getTypescriptBindings (for simple functions)', ()=> {
 	it('should generate a void function declaration', async ()=>{
 		assert.equal(
-			lastLine(await generateTypescriptBindings('lib/voidfunc.js')),
-			'declare function f(): void;'
+			findFunctions(await generateTypescriptBindings('lib/voidfunc.js')),
+			'\tf(): void;'
 		)
 	} )
 	it('should generate a void function declaration for non-modularized', 
 	async ()=>{
 		assert.equal(
-			lastLine(await generateTypescriptBindings('lib/nonmodule.js')),
-			'declare function f(): void;'
+			findFunctions(await generateTypescriptBindings('lib/nonmodule.js')),
+			'\tf(): void;'
 		)
 	} )
 	it('should generate a void function declaration after optimization',
 	async ()=>{
 		assert.equal(
-			lastLine(await generateTypescriptBindings('lib/optimized.js')),
-			'declare function f(): void;'
+			findFunctions(await generateTypescriptBindings('lib/optimized.js')),
+			'\tf(): void;'
 		)
 	} )
 	it('should generate a declaration for a function returning an integer', 
 		async ()=>{
 			assert.equal(
-				lastLine(await generateTypescriptBindings('lib/return_int.js')),
-				'declare function f(): Int;'
+				findFunctions(await generateTypescriptBindings('lib/return_int.js')),
+				'\tf(): Int;'
 			)
 		}
 	)
 	it('should generate a declaration for a function returning a float', 
 		async ()=>{
 			assert.equal(
-				lastLine(await generateTypescriptBindings('lib/return_float.js')),
-				'declare function f(): Float;'
+				findFunctions(await generateTypescriptBindings('lib/return_float.js')),
+				'\tf(): Float;'
 			)
 		}
 	)
 	it('should generate a declaration for a function returning a bool', 
 		async ()=>{
 			assert.equal(
-				lastLine(await generateTypescriptBindings('lib/return_bool.js')),
-				'declare function f(): boolean;'
+				findFunctions(await generateTypescriptBindings('lib/return_bool.js')),
+				'\tf(): boolean;'
 			)
 		}
 	)
 	it('should generate a declaration for a function returning a string', 
 		async ()=>{
 			assert.equal(
-				lastLine(await generateTypescriptBindings('lib/return_string.js')),
-				'declare function f(): string;'
+				findFunctions(await generateTypescriptBindings('lib/return_string.js')),
+				'\tf(): string;'
 			)
 		}
 	)
 	it('should generate a declaration for an int identity function',
 		async ()=>{
 			assert.equal(
-				lastLine(await generateTypescriptBindings('lib/identity.js')),
-				'declare function f(arg0: Int): Int;'
+				findFunctions(await generateTypescriptBindings('lib/identity.js')),
+				'\tf(arg0: Int): Int;'
 			)
 		}
 	)
 	it('should generate a declaration for an int sum function',
 		async ()=>{
 			assert.equal(
-				lastLine(await generateTypescriptBindings('lib/sum.js')),
-				'declare function f(arg0: Int, arg1: Int): Int;'
+				findFunctions(await generateTypescriptBindings('lib/sum.js')),
+				'\tf(arg0: Int, arg1: Int): Int;'
 			)
 		}
 	)
 	it('should generate declarations for function overloads',
 		async ()=>{
 			assert.equal(
-				lastLines(2)
+				findFunctions
 					(await generateTypescriptBindings('lib/overload.js')),
-				'declare function f(): void;\n'+
-				'declare function f(arg0: Int): void;'
+				'\tf(): void;\n'+
+				'\tf(arg0: Int): void;'
 			)
 		}
 	)
